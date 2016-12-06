723de6cb4cc00e5715f7462fbd018e237aaa7aa6
Artem Konovalenkov <artem@spotify.com>
2016-12-06 17:49:11 +0100

    add vscode settings

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.ideavimrc ~/.ideavimrc
 ln -s $PWD/.tmux.conf ~/.tmux.conf
 ln -s $PWD/.tern-config ~/.tern-config
+ln -s $PWD/vscode/settings.json ~/.config/Code/User/settings.json
