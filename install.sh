f693e0f9a9f45de79f7546d097537939de50d671
Artem Konovalenkov <artem@spotify.com>
2018-04-16 11:56:19 +0200

    add ranger

--- install.sh
+++ install.sh
@@ -5,4 +5,14 @@ ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/.ideavimrc ~/.ideavimrc
 ln -s $PWD/.tmux.conf ~/.tmux.conf
 ln -s $PWD/.tern-config ~/.tern-config
+
+mkdir --parents ~/.config/Code/User
 ln -s $PWD/vscode/settings.json ~/.config/Code/User/settings.json
+
+
+mkdir --parents ~/.config/ranger
+ln -s $PWD/ranger/commands.py ~/.config/ranger/commands.py
+ln -s $PWD/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -s $PWD/ranger/rifle.conf ~/.config/ranger/rifle.conf
+ln -s $PWD/ranger/scope.sh ~/.config/ranger/scope.sh
+
