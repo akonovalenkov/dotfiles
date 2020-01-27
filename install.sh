6004b4c84dc3e45a43b415429aff9a8accd7f51e
Artem Konovalenkov <artem@spotify.com>
2020-01-27 12:20:40 +0100

    improve dotfiles

--- install.sh
+++ install.sh
@@ -1,28 +1,37 @@
-#!/bin/bash
+#!/bin/sh
 
-ln -s $PWD/.vimrc ~/.vimrc
-ln -s $PWD/.zshrc ~/.zshrc
-ln -s $PWD/.gitconfig ~/.gitconfig
-ln -s $PWD/.tigrc ~/.tigrc
-ln -s $PWD/.ideavimrc ~/.ideavimrc
-ln -s $PWD/.tmux.conf ~/.tmux.conf
-ln -s $PWD/.tern-config ~/.tern-config
+apt update
 
-mkdir --parents ~/.config/Code/User
-ln -s $PWD/vscode/settings.json ~/.config/Code/User/settings.json
-ln -s $PWD/vscode/settings.json ~/.config/Code/User/keybindings.json
+apt install wget vim ranger tmux zsh silversearcher-ag tig
 
-mkdir --parents ~/.config/ranger
-ln -s $PWD/ranger/commands.py ~/.config/ranger/commands.py
-ln -s $PWD/ranger/rc.conf ~/.config/ranger/rc.conf
-ln -s $PWD/ranger/rifle.conf ~/.config/ranger/rifle.conf
-ln -s $PWD/ranger/scope.sh ~/.config/ranger/scope.sh
-
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-sudo apt install ranger tmux zsh silversearcher-ag tig
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+git clone --depth 1 https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+ln -sf $PWD/.vimrc-plugins ~/.vimrc-plugins
+ln -sf $PWD/.vimrc ~/.vimrc
+
+vim -u ~/.vimrc-plugins +PluginInstall +qall
+
+ln -sf $PWD/.zshrc ~/.zshrc
+ln -sf $PWD/.gitconfig ~/.gitconfig
+ln -sf $PWD/.tigrc ~/.tigrc
+ln -sf $PWD/.ideavimrc ~/.ideavimrc
+ln -sf $PWD/.tmux.conf ~/.tmux.conf
+ln -sf $PWD/.tern-config ~/.tern-config
+
+mkdir --parents ~/.config/Code/User
+ln -sf $PWD/vscode/settings.json ~/.config/Code/User/settings.json
+ln -sf $PWD/vscode/settings.json ~/.config/Code/User/keybindings.json
+
+mkdir --parents ~/.config/ranger
+ln -sf $PWD/ranger/commands.py ~/.config/ranger/commands.py
+ln -sf $PWD/ranger/rc.conf ~/.config/ranger/rc.conf
+ln -sf $PWD/ranger/rifle.conf ~/.config/ranger/rifle.conf
+ln -sf $PWD/ranger/scope.sh ~/.config/ranger/scope.sh
+
 
 export FZF_DEFAULT_COMMAND='rg --files';
