b0525f7e405d2f8a2af4027a8c0456a73394c773
artem <some@some>
2023-05-27 23:22:26 +0200

    tweak nvim

--- nvim/lua/art/plugins/tree.lua
+++ nvim/lua/art/plugins/tree.lua
@@ -19,23 +19,14 @@ local plugin = {
       require('nvim-tree.api').tree.toggle({focus = false, find_find = true})
     end, {})
 
-    vim.api.nvim_create_autocmd({ "VimEnter" }, {
-      callback = function(data)
-        -- buffer is a real file on the disk
-        local real_file = vim.fn.filereadable(data.file) == 1
+    local config_group = vim.api.nvim_create_augroup('MyConfigGroup', {})
 
-        -- buffer is a [No Name]
-        local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
-
-        if not real_file and not no_name then
-          -- return
-        end
-
-        -- open the tree, find the file but don't focus it
-        -- require("nvim-tree.api").tree.toggle({ focus = false, find_file = true, })
-
-        -- require('nvim-tree.api').tree.open({focus = true })
-      end
+    vim.api.nvim_create_autocmd({ 'User' }, {
+      pattern = "SessionLoadPost",
+      group = config_group,
+      callback = function()
+        require('nvim-tree.api').tree.toggle({focus = false, find_file = true})
+      end,
     })
   end
 }
