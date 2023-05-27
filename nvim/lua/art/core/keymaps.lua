a524ecf04fad58085721ad5237ea4b9ab54007ff
artem <some@some>
2023-05-27 13:06:58 +0200

    tweak nvim

--- nvim/lua/art/core/keymaps.lua
+++ nvim/lua/art/core/keymaps.lua
@@ -1,4 +1,5 @@
 vim.keymap.set({"i", "v"}, ";", "<esc>")
-vim.keymap.set({"n", "v"}, "<leader>q", ":confirm qall<cr>")
+vim.keymap.set({"n", "v"}, "<leader>qq", ":confirm qall<cr>")
+vim.keymap.set({"n", "v"}, "<leader>q", ":confirm bd<cr>")
 vim.keymap.set("n", "0", "^")
 
